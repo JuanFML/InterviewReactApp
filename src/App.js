d4e347cb74b9b749dede2257f32e685cefc90a24
Juan Medina <jfml_97@live.com>
2021-02-18 15:27:19 -0600

    last message

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>Please search below.</p>
+        <p>
+          Although it works I wasn't able to filter directly on the API only
+          here :( , so that is why the list of issues is short.
+        </p>
         <IssuesList></IssuesList>
       </header>
     </div>
