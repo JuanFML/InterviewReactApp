59ce2da8f50d4d52508fa53b335362523ad5683e
JMEDINA <juan.medina@encora.com>
2021-07-29 13:45:12 -0500

    m

--- src/issues.js
+++ src/issues.js
@@ -7,11 +7,7 @@ const IssuesList = () => {
 
   useEffect(() => {
     fetch(
-      "http://internal-loadbalancer-backend-1034007956.us-east-2.elb.amazonaws.com",
-      // `https://api.github.com/repos/facebook/react/issues?per_page=100&&page=${page}`,
-      {
-        method: "GET",
-      }
+      "http://internal-loadbalancer-backend-1034007956.us-east-2.elb.amazonaws.com"
     )
       .then((res) => {
         console.log(res);
