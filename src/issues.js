793d456c25bb35f3b70501f6d95d92873ebd9454
JMEDINA <juan.medina@encora.com>
2021-07-29 15:43:02 -0500

    d

--- src/issues.js
+++ src/issues.js
@@ -7,7 +7,7 @@ const IssuesList = () => {
 
   useEffect(() => {
     fetch(
-      "http://internal-loadbalancer-backend-1658632040.us-east-2.elb.amazonaws.com"
+      "http://internal-loadbalancer-backend-1658632040.us-east-2.elb.amazonaws.com:80"
     ).then((res) => {
       res.json().then((result) => {
         setItems(result);
